298d7a22cae5dd658f89a4a532bb2976bc63fcda
Tern321 <55168319+Tern321@users.noreply.github.com>
2021-01-06 13:21:05 +0300

    Copy button for mobile devices

--- Forum/scripts/Controller.ts
+++ Forum/scripts/Controller.ts
@@ -189,6 +189,66 @@ class Controller {
         document.getElementById("loginTextArea").style.width = "400px";
         document.getElementById("encriptionKeyTextArea").style.width = "400px";
     }
+    static copy() {
+        var text = "";
+        if (Controller.selectedcontention().url == undefined) {
+            text = Controller.selectedcontention().text;
+        }
+        else {
+            text = Controller.selectedcontention().url;
+        }
+
+
+        var w: any = window;
+        w.Clipboard = (function (window, document, navigator) {
+            var textArea,
+                copy;
+
+            function isOS() {
+                return navigator.userAgent.match(/ipad|iphone/i);
+            }
+
+            function createTextArea(text) {
+                textArea = document.createElement('textArea');
+                textArea.value = text;
+                document.body.appendChild(textArea);
+            }
+
+            function selectText() {
+                var range,
+                    selection;
+
+                if (isOS()) {
+                    range = document.createRange();
+                    range.selectNodeContents(textArea);
+                    selection = window.getSelection();
+                    selection.removeAllRanges();
+                    selection.addRange(range);
+                    textArea.setSelectionRange(0, 999999);
+                } else {
+                    textArea.select();
+                }
+            }
+
+            function copyToClipboard() {
+                document.execCommand('copy');
+                document.body.removeChild(textArea);
+            }
+
+            copy = function (text) {
+                createTextArea(text);
+                selectText();
+                copyToClipboard();
+            };
+
+            return {
+                copy: copy
+            };
+        })(w, document, navigator);
+
+        var c: any = Clipboard;
+        c.copy(text);
+    }
 }
 
 function download(filename, text) {
